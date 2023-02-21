a6574e9cf0af07826264ce4ce4c1d9410e370fe2
PerfectlyFineCode <70201838+PerfectlyFineCode@users.noreply.github.com>
2023-02-21 13:56:27 +0100

    initial repository

---
+++ src/RecipesManager/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace RecipesManager;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ src/RecipesManager/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace RecipesManager;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
